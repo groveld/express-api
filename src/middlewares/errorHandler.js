94760d7ec84ce5806b31fb696dcb814361b383c2
Martin Groeneveld <3328493+groveld@users.noreply.github.com>
2023-03-24 14:18:14 +0100

    update logger and error handler

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -2,32 +2,27 @@
 const util = require('util');
 const logger = require('../utils/logger');
 
-function createOpenApiErrorPayload(err) {
-  return {
-    code: err.status || 500,
-    message: err.message,
-    errors: err.errors,
-  };
-}
-
-function errorHandlerMiddleware(err, req, res, next) {
-  const isClientError = err.status >= 400 && err.status < 500;
-
-  if (!isClientError) {
-    console.log(util.inspect(err, { depth: null, colors: true }));
-  }
-
-  const errorPayload = createOpenApiErrorPayload(err);
-
-  logger.error(`Error: ${err.message}`, {
+const errorHandlerMiddleware = (err, req, res, next) => {
+  // Log the error to Logtail
+  logger.error('Server error', {
     requestId: req.requestId,
     info: {
+      error: err.message,
+      stack: err.stack,
       ip: req.ip,
       userAgent: req.headers['user-agent'],
     },
   });
 
-  res.status(errorPayload.code).json(errorPayload);
-}
+  // Check if the error is from express-openapi-validator
+  if (err instanceof SyntaxError && 'body' in err) {
+    return res.status(400).json({ error: 'Invalid JSON body' });
+  }
+
+  // Handle other errors
+  res.status(err.status || 500).json({
+    error: err.message || 'An internal server error occurred',
+  });
+};
 
 module.exports = errorHandlerMiddleware;
--- src/utils/logger.js
+++ src/utils/logger.js
@@ -1,30 +1,23 @@
 // src/utils/logger.js
 const { createLogger, format, transports } = require('winston');
+const { Logtail } = require('@logtail/node');
+const { LogtailTransport } = require('@logtail/winston');
+
+const loggerOptions = {
+  level: 'debug',
+  format: format.combine(format.timestamp(), format.json()),
+};
+
+const transportOptions = [new transports.Console()];
+
+if (process.env.NODE_ENV == 'production') {
+  const logtail = new Logtail('5MdyLnQpkYwtiBiLxJfwGzFW');
+  transportOptions.push(new LogtailTransport(logtail));
+}
 
 const logger = createLogger({
-  transports: [
-    new transports.Console({
-      level: 'silly',
-      format: format.combine(
-        format.colorize(),
-        format.align(),
-        format.timestamp(),
-        format.printf(({ timestamp, level, message, requestId }) => {
-          return `${timestamp} ${requestId} ${level} ${message}`;
-        }),
-      ),
-    }),
-    new transports.File({
-      filename: 'app-error.log',
-      level: 'error',
-      format: format.combine(format.timestamp(), format.json()),
-    }),
-    new transports.File({
-      filename: 'app-debug.log',
-      level: 'debug',
-      format: format.combine(format.timestamp(), format.json()),
-    }),
-  ],
+  loggerOptions,
+  transports: transportOptions,
 });
 
 module.exports = logger;

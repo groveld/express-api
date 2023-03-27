fae589612a46a07065450b7e9cab0c8ea4bd7978
Martin Groeneveld <3328493+groveld@users.noreply.github.com>
2023-03-27 15:30:24 +0200

    added database

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,53 +1,73 @@
 // File: src/controllers/authController.js
 
 const jwt = require('jsonwebtoken');
-// const bcrypt = require('bcryptjs');
+const bcrypt = require('bcryptjs');
+const User = require('../models/userModel');
 const logger = require('../utils/logger');
-const { users } = require('../models/userModel');
-const { jwtSecret } = require('../config');
+const { jwtSecret, jwtRefreshSecret } = require('../config');
 
 exports.login = async (req, res) => {
-  const requestId = req.requestId;
-  logger.debug('Request received: GET /auth/login', { requestId });
-
   const { username, password } = req.body;
-  const user = users.find(u => u.username === username);
 
-  if (!user) {
-    return res.status(401).json({ message: 'User not found' });
-  }
+  const user = await User.findOne({ where: { username } });
 
-  // const passwordMatches = await bcrypt.compare(password, user.password);
-  const passwordMatches = password === user.password;
-
-  if (!passwordMatches) {
-    return res.status(401).json({ message: 'Incorrect username or password' });
+  if (!user || !(await bcrypt.compare(password, user.password))) {
+    return res.status(401).json({ message: 'Invalid credentials' });
   }
 
   const access_token = jwt.sign({ userId: user.id }, jwtSecret, {
     expiresIn: '1h',
   });
-  const refresh_token = jwt.sign({ userId: user.id }, jwtSecret, {
-    expiresIn: '2h',
+
+  const refresh_token = jwt.sign({ userId: user.id }, jwtRefreshSecret, {
+    expiresIn: '7d',
   });
 
   res.json({ message: 'Authenticated', access_token, refresh_token });
 };
 
 exports.refresh = async (req, res) => {
-  const requestId = req.requestId;
-  logger.debug('Request received: GET /auth/refresh', { requestId });
-  // Placeholder for token refresh logic
+  const { refresh_token } = req.body;
+
+  try {
+    const decoded = jwt.verify(refresh_token, jwtRefreshSecret);
+    const user = await User.findOne({ where: { id: decoded.userId } });
+
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+
+    const access_token = jwt.sign({ userId: user.id }, jwtSecret, {
+      expiresIn: '1h',
+    });
+
+    res.json({ access_token });
+  } catch (err) {
+    res.status(401).json({ message: 'Invalid or expired refresh token' });
+  }
+};
+
+exports.register = async (req, res) => {
+  const { username, email, password } = req.body;
+
+  try {
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const newUser = await User.create({
+      username,
+      email,
+      password: hashedPassword,
+    });
+
+    res.status(201).json({ message: 'User created', user_id: newUser.id });
+  } catch (err) {
+    res
+      .status(400)
+      .json({ message: 'Registration failed', error: err.message });
+  }
 };
 
 exports.logout = async (req, res) => {
   const requestId = req.requestId;
   logger.debug('Request received: GET /auth/logout', { requestId });
   // Placeholder for user logout logic
-};
-
-exports.register = async (req, res) => {
-  const requestId = req.requestId;
-  logger.debug('Request received: GET /auth/register', { requestId });
-  // Placeholder for user registration logic
 };

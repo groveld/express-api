0cf2d5b84a4824205a09630a4850fb4f51a62b60
Martin Groeneveld <3328493+groveld@users.noreply.github.com>
2023-03-27 09:48:14 +0200

    updated prettier

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -2,10 +2,11 @@
 // Docs: https://prettier.io/docs/en/configuration.html
 
 module.exports = {
+  printWidth: 80,
+  endOfLine: 'lf',
+  arrowParens: 'avoid',
   trailingComma: 'all',
   tabWidth: 2,
   semi: true,
   singleQuote: true,
-  printWidth: 80,
-  arrowParens: 'avoid',
 };

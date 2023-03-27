a68f19e64df622ff84f230d3e5d658010a873074
Martin Groeneveld <3328493+groveld@users.noreply.github.com>
2023-03-27 11:05:50 +0200

    update eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,13 +2,27 @@
 // Docs: https://eslint.org/docs/latest/use/configure/configuration-files
 
 module.exports = {
-  extends: ['prettier', 'eslint:recommended'],
+  root: true,
+  extends: ['eslint:recommended', 'plugin:prettier/recommended'],
   plugins: ['prettier'],
   env: {
     browser: true,
     es2022: true,
     jest: true,
+    node: true,
   },
-  parserOptions: {},
-  rules: {},
+  parserOptions: {
+    ecmaVersion: 2022,
+    sourceType: 'module',
+  },
+  rules: {
+    'prettier/prettier': 'error',
+    'no-console': 'warn',
+    'no-unused-vars': 'warn',
+    'no-var': 'error',
+    'prefer-const': 'error',
+    eqeqeq: ['error', 'always'],
+    quotes: ['error', 'single', { allowTemplateLiterals: true }],
+    semi: ['error', 'always'],
+  },
 };
